9e531192e072b6f7548bfb11ae5fa466fe255777
gtukrdscrjy <133485190+gtukrdscrjy@users.noreply.github.com>
2023-05-13 06:58:19 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=e4c40e74-8352-4bba-a36c-10349334eba4
+export UUID=7a0747e3-d482-4b11-b030-fe08dca14307
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=techtanker.com
+export ProxySite=arsenalfclatestnews.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
